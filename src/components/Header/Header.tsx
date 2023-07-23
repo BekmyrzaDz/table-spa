b3c5697eb0fdcc2178563cd597fd32e10c2100d7
Bekmyrza <msn.10110922@gmail.com>
2023-07-23 18:33:38 +0600

    Fix Header component types errors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
-import {FC, useState} from 'react'
+import {FC} from 'react'
 import {Input} from '../Input/MyInput.tsx'
 import styles from './Header.module.scss'
 
 export const Header: FC = () => {
-  const [value, setValue] = useState('')
-
   return (
     <div className={styles.wrapper}>
       <Input name={'input'} placeholder={'Поиск'}/>
